755329807a56c39e07e9e87455347639f1a99681
volga <volga@DESKTOP-89UV4JU>
2023-05-21 23:11:12 +0500

    111

--- HomeWork 20/Functions.cpp
+++ HomeWork 20/Functions.cpp
@@ -28,3 +28,46 @@ int search_index(int arr[], const int length, int search, int index) {
 		
 			return -2;
 }
+int arr_min(int arr[], const int length) {
+	int min = arr[0];
+	for (int i = 0; i < length; i++){
+		if (arr[i] < min)
+			min = arr[i];
+	}
+	return min;
+}
+
+int arr_max(int arr[], const int length) {
+	int max = arr[0];
+	for (int i = 0; i < length; i++) {
+		if (arr[i] > max)
+			max = arr[i];
+	}
+	return max;
+}
+double mean_value(int arr[], const int length) {
+	double mid = 0.0;
+	for (int i = 0; i < length; i++)
+		mid += arr[i];
+	return mid / length;
+}
+void range(int arr[], const int length, int num1, int num2) {
+	if (num1<0 || num1>length)
+		std::cout << "Error!!!";
+	else if(num1>=0&&num1<=length&&num2>length)
+		for (int i = num1; i < length; i++){
+			std::cout << arr[i] << ' ';
+		}
+	else
+		for (int i = num1; i < num2; i++)	{
+			std::cout << arr[i] << ' ';
+		}
+}
+int counter(int arr[], const int length, int num1) {
+	int counter = 0;
+	for (int i = 0; i < length; i++){
+		if (arr[i] == num1)
+			counter++;
+	}
+	return counter;
+}
--- HomeWork 20/Functions.h
+++ HomeWork 20/Functions.h
@@ -7,3 +7,8 @@
 void fill_arr(int arr[], int length, int left, int right);
 void print_arr(int arr[], int length);
 int search_index(int arr[], const int length, int search, int index = 0);
+int arr_min(int arr[], const int length);
+int arr_max(int arr[], const int length);
+double mean_value(int arr[], const int length);
+void range(int arr[], const int length, int num1, int num2);
+int counter(int arr[], const int length, int num1);
